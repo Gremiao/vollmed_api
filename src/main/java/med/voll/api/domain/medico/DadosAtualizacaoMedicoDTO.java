389ba4bc87bd5e284220ee7418eab4f3e8fa5127
Gremiao <gremiao@gmail.com>
2025-01-30 06:49:50 -0300

    Commits refentes a aula de 29/01.

--- src/main/java/med/voll/api/domain/medico/DadosAtualizacaoMedicoDTO.java
+++ src/main/java/med/voll/api/domain/medico/DadosAtualizacaoMedicoDTO.java
@@ -1,9 +1,9 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.medico;
 
 import jakarta.validation.constraints.NotNull;
-import med.voll.api.endereco.DadosEnderecoDTO;
+import med.voll.api.domain.endereco.DadosEnderecoDTO;
 
-public record DadosAtualizacaoPaciente(
+public record DadosAtualizacaoMedicoDTO(
         @NotNull
         Long id,
         String nome,
